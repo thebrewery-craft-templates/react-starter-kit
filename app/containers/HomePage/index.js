01772fb19b8f72e05e194affe071793c6040c878
jabalus <jabalus@stratpoint.com>
2019-10-16 13:32:31 +0800

    change styles

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,32 +7,37 @@
 
 import React from 'react';
 import MaltLogo from 'images/malt.png';
+import RBLogo from 'images/rb-logo.png';
 import {
   StyledLayout,
-  Background,
-  BreweryTitleContainer,
-  BreweryTitleWrapper,
-  BreweryTitle,
-  BrewerySubtitle,
-  CraftLogoImage,
-  StyledFooter,
+  CenterDiv,
+  Content,
+  Links,
+  ResponsiveImg,
+  RBLogoImage,
+  RBLogoWrapper,
 } from './styles';
 // Ant Design is pre installed , use included components for faster delivery
 
 export default function HomePage() {
   return (
     <StyledLayout>
-      <Background>
-        <BreweryTitleContainer>
-          <CraftLogoImage src={MaltLogo} alt="craft logo" />
-          <BrewerySubtitle> React Boilerplate </BrewerySubtitle>
-          powered by:
-          <BreweryTitleWrapper>
-            <BreweryTitle> THE BREWERY </BreweryTitle>
-          </BreweryTitleWrapper>
-        </BreweryTitleContainer>
-      </Background>
-      <StyledFooter>Malt © 2019 Created by Stratpoint</StyledFooter>
+      <CenterDiv>
+        <Content>
+          <ResponsiveImg src={MaltLogo} />
+          <Links>
+            <a href="http://stratpoint.com/">Documentation</a>
+            <a href="http://stratpoint.com/">GitLab</a>
+            <a href="http://stratpoint.com/">Tutorials</a>
+            <a href="http://stratpoint.com/">Get Started</a>
+            <a href="http://stratpoint.com/">Lezgo!</a>
+          </Links>
+          <RBLogoWrapper>
+            <p>powered by:</p>
+            <RBLogoImage src={RBLogo} />
+          </RBLogoWrapper>
+        </Content>
+      </CenterDiv>
     </StyledLayout>
   );
 }
--- app/containers/HomePage/styles.js
+++ app/containers/HomePage/styles.js
@@ -1,63 +1,64 @@
 import styled from 'styled-components';
-import { Layout } from 'antd';
 
-const { Footer } = Layout;
-
-export const CraftLogoImage = styled.img`
-  max-width: 500px;
+export const StyledLayout = styled.div`
+  height: 100vh;
+  background: #000000;
+  color: #636b6f;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 100;
+  margin: 0;
 `;
 
-export const StyledLayout = styled(Layout)`
+export const CenterDiv = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  position: relative;
   height: 100vh;
 `;
-export const StyledFooter = styled(Footer)`
-  color: #ffffff;
-  background: #181818;
-  text-align: center;
-`;
 
-export const Background = styled.div`
-  flex-grow: 1;
-  width: 100%;
-  background: #4f4f50;
-  color: #ffffff;
-  display: flex;
-  justify-content: center;
+export const Content = styled.div`
   align-items: center;
-`;
-
-export const BreweryTitleWrapper = styled.div`
-  border-top: 2px solid #ffffff;
-  border-bottom: 2px solid #ffffff;
-  transform: skewX(-15deg);
-  width: 456px;
-  white-space: nowrap;
-  overflow: visible;
   display: flex;
   justify-content: center;
-  margin-left: 22px;
-`;
-
-export const BreweryTitleContainer = styled.div`
   position: relative;
-  display: flex;
+  height: 100vh;
   flex-direction: column;
-  align-items: center;
 `;
 
-export const BreweryTitle = styled.h1`
-  height: 30px;
-  line-height: 30px;
-  font-size: 24px;
-  font-weight: 500;
-  color: #f9cc41;
-  margin: 0;
-  letter-spacing: 15px;
+export const Links = styled.div`
+  & > a {
+    color: #ffffff;
+    padding: 0 25px;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
 `;
 
-export const BrewerySubtitle = styled.h2`
-  font-weight: 300;
-  font-size: 45px;
-  color: #ffffff;
-  letter-spacing: 5px;
+export const ResponsiveImg = styled.img`
+  max-width: 55%;
+  height: auto;
+  margin-bottom: 30px;
+`;
+
+export const RBLogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+  p {
+    margin-right: 1.25em;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
+
+export const RBLogoImage = styled.img`
+  max-width: 25%;
+  height: auto;
 `;
